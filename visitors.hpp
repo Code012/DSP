7241a7bf11880f4a19beef5ab195097cad874fb3
Shahbaz Bokhari <syedshahbazhaider007@gmail.com>
2025-02-24 05:18:23 +0000

    Implemented visitor pattern, and its first application in terms of a basic association operator for * and + nodes

--- visitors.hpp
+++ visitors.hpp
@@ -2,12 +2,17 @@
 18/2/25
 */
 
+#ifndef VISITOR_CPP
+#define VISITOR_CPP
+
+class ExprVisitor;
+class ExprMutableVisitor;
+class AssociativeTransformation;
+
 #include "ast.hpp"
 
-class NumberExpressionNode;
-class VariableExpressionNode;
-class PrefixExpressionNode;
-class InfixExpressionNode;
+
+
 
 // Base Visitor (Mutable for Simplification stage where ast will be transformed)
 struct ExprMutableVisitor {
@@ -18,6 +23,8 @@ struct ExprMutableVisitor {
     virtual void visit(VariableExpressionNode&) = 0;
     virtual void visit(PrefixExpressionNode&) = 0;
     virtual void visit(InfixExpressionNode&) = 0;
+    virtual void visit(NaryExpressionNode&) = 0;
+
 };
 
 struct ExprVisitor {
@@ -27,5 +34,25 @@ struct ExprVisitor {
     virtual void visit(const NumberExpressionNode&) = 0;
     virtual void visit(const VariableExpressionNode&) = 0;
     virtual void visit(const PrefixExpressionNode&) = 0;
-    virtual void visit(const InfixExpressionNode&) = 0;
+    virtual void visit(const InfixExpressionNode &) = 0;
+    virtual void visit(const NaryExpressionNode &) = 0;
+
 };
+
+// -------------CAS FUNCTIONS--------------------
+
+// Flattening nested operators, ((((2 + 2) + 3) * 4 ) * 4 ) -> ((2 + 2 + 3) * 4 * 4)
+class AssociativeTransformationVisitor : public ExprMutableVisitor {
+    public:
+        void visit(NumberExpressionNode& node) override;
+        void visit(VariableExpressionNode& node) override;
+        void visit(PrefixExpressionNode& node) override;
+        void visit(InfixExpressionNode& node) override;
+        void visit(NaryExpressionNode& node) override;
+    
+    private: 
+        void flattenOperands(std::unique_ptr<ExpressionNode>& node, InfixKind kind, std::vector<std::unique_ptr<ExpressionNode>>& flattenedOperands);
+           
+};
+    
+#endif        
